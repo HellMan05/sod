815177224206e19e3db6fd4beb9ebe646b13b587
Bork Roman <157475743+borkroman@users.noreply.github.com>
2025-07-15 11:42:04 +0700

    Fixes and Cargo locale (#378)
    1. Сигматизм теперь поход на НОРМАЛЬНЫЙ сигматизм
    2. переведён пиратский акцент (так как оригинал визов имел в себе слова, которые ну никак не переделать на русский, пришлось импровизировать, но вышло заебись
    3. переведены некоторые задачи карго
    4. полностью переведена консоль утильских рангов
    5. утильская цель на повелителя роя заменена на сердце легиона

--- Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
+++ Content.Server/Speech/EntitySystems/FrontalLispSystem.cs
@@ -36,18 +36,24 @@ public sealed class FrontalLispSystem : EntitySystem
         // с - ш
         message = Regex.Replace(message, @"с", _random.Prob(0.90f) ? "ш" : "с");
         message = Regex.Replace(message, @"С", _random.Prob(0.90f) ? "Ш" : "С");
-        // ч - ш
-        message = Regex.Replace(message, @"ч", _random.Prob(0.90f) ? "ш" : "ч");
-        message = Regex.Replace(message, @"Ч", _random.Prob(0.90f) ? "Ш" : "Ч");
-        // ц - ч
-        message = Regex.Replace(message, @"ц", _random.Prob(0.90f) ? "ч" : "ц");
-        message = Regex.Replace(message, @"Ц", _random.Prob(0.90f) ? "Ч" : "Ц");
-        // т - ч
-        message = Regex.Replace(message, @"\B[т](?![АЕЁИОУЫЭЮЯаеёиоуыэюя])", _random.Prob(0.90f) ? "ч" : "т");
-        message = Regex.Replace(message, @"\B[Т](?![АЕЁИОУЫЭЮЯаеёиоуыэюя])", _random.Prob(0.90f) ? "Ч" : "Т");
-        // з - ж
-        message = Regex.Replace(message, @"з", _random.Prob(0.90f) ? "ж" : "з");
-        message = Regex.Replace(message, @"З", _random.Prob(0.90f) ? "Ж" : "З");
+        // ч - тьш
+        message = Regex.Replace(message, @"ч", _random.Prob(0.90f) ? "тьш" : "ч");
+        message = Regex.Replace(message, @"Ч", _random.Prob(0.90f) ? "ТЬШ" : "Ч");
+        // ц - тс
+        message = Regex.Replace(message, @"ц", _random.Prob(0.90f) ? "тс" : "ц");
+        message = Regex.Replace(message, @"Ц", _random.Prob(0.90f) ? "ТС" : "Ц");
+        // т - тч
+        message = Regex.Replace(message, @"т", _random.Prob(0.90f) ? "тч" : "т");
+        message = Regex.Replace(message, @"Т", _random.Prob(0.90f) ? "ТЧ" : "Т");
+        // з - жь
+        message = Regex.Replace(message, @"з", _random.Prob(0.90f) ? "жь" : "з");
+        message = Regex.Replace(message, @"З", _random.Prob(0.90f) ? "ЖЬ" : "З");
+        // щ - шь
+        message = Regex.Replace(message, @"щ", _random.Prob(0.90f) ? "шь" : "щ");
+        message = Regex.Replace(message, @"Щ", _random.Prob(0.90f) ? "ШЬ" : "Щ");
+        // ж - щь
+        message = Regex.Replace(message, @"ж", _random.Prob(0.90f) ? "щь" : "ж");
+        message = Regex.Replace(message, @"Ж", _random.Prob(0.90f) ? "ЩЬ" : "Ж");
         // Adventure social anxiety end
 
         args.Message = message;
